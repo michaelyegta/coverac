70c01ea809d8ea4b26135cfad949afbd79223670
Michael Ye <michaelye.gta@gmail.com>
2018-11-16 23:41:59 -0500

    created instrumented test skeleton

---
+++ app/src/androidTest/java/com/example/michaelyegta/autocomplete/CarrierFragmentTest.kt
@@ -0,0 +1,26 @@
+package com.example.michaelyegta.autocomplete
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CarrierFragmentTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun onCreateView() {
+    }
+
+    @Test
+    fun onViewCreated() {
+    }
+}
